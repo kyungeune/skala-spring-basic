d160fe0e82338ca99b44c981514fc00a1e39ad04
kyungeune <kyungeunhehe@gmail.com>
2026-01-27 11:41:34 +0900

    add replaced SpringBootApplication

--- src/main/java/com/skala/basic/BasicApplication.java
+++ src/main/java/com/skala/basic/BasicApplication.java
@@ -2,8 +2,14 @@ package com.skala.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringBootConfiguration;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @SpringBootConfiguration
+// @EnableAutoConfiguration
+// @ComponentScan(basePackages = {"com.skala/basic"})
 public class BasicApplication {
 
 	public static void main(String[] args) {
